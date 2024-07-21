0278d1da3394e21b6130a5edbf32885e37abb273
RexuDev <102574307+RexuDev@users.noreply.github.com>
2024-07-22 07:57:02 +0800

    added fullscreen and mobile homescreen

--- webdevA2/script.js
+++ webdevA2/script.js
@@ -31,6 +31,48 @@ function getRndCol() {
 	return rndCol;
 }
 
+//fullscreen toggle
+const fstoggle = document.querySelector("#fstoggle");
+var togglefs = false;
+function enterFullscreen() {
+if (document.documentElement.requestFullscreen) {
+document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) { // Firefox
+document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+document.documentElement.webkitRequestFullscreen();
+} else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+document.documentElement.msRequestFullscreen();
+}
+}
+// Function to exit fullscreen mode
+function exitFullscreen() {
+if (document.exitFullscreen) {
+document.exitFullscreen();
+} else if (document.mozCancelFullScreen) { // Firefox
+document.mozCancelFullScreen();
+} else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+document.webkitExitFullscreen();
+} else if (document.msExitFullscreen) { // IE/Edge
+document.msExitFullscreen();
+}
+}
+function toggleFS()
+{
+	if (togglefs)
+	{
+		togglefs = false;
+		exitFullscreen();
+	}
+	else
+	{
+		togglefs = true;
+		enterFullscreen();
+	}
+}
+fstoggle.addEventListener("click",toggleFS)
+fstoggle.addEventListener("touchstart", toggleFS)
+
 /*Listen for clicks on the buttons, assign anonymous
 eventhandler functions to call show function*/
 page1btn.addEventListener("click", function() {
